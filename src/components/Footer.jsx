6f513bbabe21ec368a70d7f511f4f40c49b03726
MonjedNidal <monjed1010@gmail.com>
2023-08-07 14:14:45 +0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <div class="attribution">
       Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-      Coded by <a href="https://github.com/monjednidal">MonjedNidal</a>.
+      <a href="https://www.frontendmentor.io?ref=challenge"> Frontend Mentor</a>
+      . Coded by <a href="https://github.com/monjednidal">MonjedNidal</a>.
     </div>
   );
 }
